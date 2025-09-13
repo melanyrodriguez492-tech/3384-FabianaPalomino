edca31150878bd4d0d457b772de8cd6d6fd9bea2
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-13 17:44:37 -0500

    Creando mi primer ejercicio de Python Condicional Multiple

---
+++ 02_EstructurasControl/Secuenciales/Python/Ejercicio1.py
@@ -0,0 +1,9 @@
+# Ejemplo: Clasificar un número según su valor
+
+numero = int(input("Ingresa un número: "))
+if numero > 0:
+    print("El número es positivo.")
+elif numero == 0:
+    print("El número es cero.")
+else:
+    print("El número es negativo.")
