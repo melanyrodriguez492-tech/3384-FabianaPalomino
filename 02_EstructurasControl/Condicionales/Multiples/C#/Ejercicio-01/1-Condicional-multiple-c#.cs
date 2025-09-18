24893c28cb5b2dff91995e72b0d1f6b50951a8ae
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-18 15:33:10 -0500

    Corregir ejercicio de c#

---
+++ 02_EstructurasControl/Condicionales/Multiples/C#/Ejercicio-01/1-Condicional-multiple-c#.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3384_FABIANAPALOMINO_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //# Ejemplo: Clasificar un número según su valor
+            Console.WriteLine("Ingrese un número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_EstructurasControl/Condicionales/Multiples/Python/README.md
@@ -0,0 +1,17 @@
+
+## Explicación del ejercicio 01: Estructura condicional múltiple en Python
+   
+   # Este programa solicita al usuario que ingrese un número entero y luego clasifica ese número en tres categorías: positivo, cero o negativo.
+   
+   # La estructura condicional múltiple se implementa usando las sentencias if, elif y else:
+   
+   # if numero > 0:
+   
+   # Verifica si el número es mayor que cero. Si esta condición es verdadera, el programa imprime que el número es positivo.
+   
+   # elif numero == 0:
+   
+   # Si la primera condición no se cumple, esta línea verifica si el número es igual a cero. Si es así, imprime que el número es cero.
+   
+   # else:
+   # Si ninguna de las condiciones anteriores se cumple, significa que el número es menor que cero, por lo que el programa imprime que el número es negativo.
