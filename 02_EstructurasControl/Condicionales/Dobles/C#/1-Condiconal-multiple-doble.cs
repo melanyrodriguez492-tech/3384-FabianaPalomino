65512162467333b655856b445eaec5615cdf85e9
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-18 16:37:13 -0500

    Agregando el ejemplo de condional doble en c# y Python

---
+++ 02_EstructurasControl/Condicionales/Dobles/C#/1-Condiconal-multiple-doble.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicional_Doble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa condiconal doble para evaluar si una persona tiene licencia de conducir y depende a eso decirle si puede o no manejar
+            Console.WriteLine("¿Tienes licencia de conducir? (si/no):");
+            string respuesta = Console.ReadLine().ToLower();
+            if (respuesta == "si")
+            {
+                Console.WriteLine("Puedes manejar.");
+            }
+            else
+            {
+                Console.WriteLine("No puedes manejar.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_EstructurasControl/Condicionales/Dobles/Python/Ejercicio01.py
@@ -0,0 +1,6 @@
+
+respuesta = input("¿Tienes licencia de conducir? (sí/no): ").lower()
+if respuesta == "sí":
+    print("Puedes conducir un vehículo.")
+else:
+    print("No puedes conducir un vehículo.")
