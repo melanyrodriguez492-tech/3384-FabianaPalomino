2dda29bf4a05a2ccb116d12a11fcd7711f5ad652
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-21 12:56:33 -0500

    Haciendo el ejercicio 02 en Python condicional doble

---
+++ 02_EstructurasControl/Condicionales/Dobles/Python/Ejerccio02.py
@@ -0,0 +1,8 @@
+# Pedir al usuario la temperatura actual en grados Celsius
+temperatura = int(input("Ingrese la temperatura actual en grados Celsius: "))
+
+# Evaluar la temperatura y mostrar el mensaje correspondiente
+if temperatura > 25:
+    print("¡Es un día de helado!")
+else:
+    print("Mejor prepara un café caliente.")
