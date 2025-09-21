7ea80c0a54e2b9a82db4b8e65c9a9a5300c5d2b0
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-21 16:57:14 -0500

    Haciendo el ejemplo 01 de estructura repetitiva For en c# y python

---
+++ 02_EstructurasControl/Repetitivas/For/C#/Ejemplo-01/Ejemplo01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pide al usuario un número y genera su tabla de multiplicar hasta 10.
+            Console.WriteLine("Ingrese un número para generar su tabla de multiplicar:");
+            int numero = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
