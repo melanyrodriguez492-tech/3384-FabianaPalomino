7184b291064656f4623fca42b97b52c21e85b2bd
Fabiana Melany <melanyrodriguez492@gmail.com>
2025-09-22 19:25:38 -0500

    agreagando los ejercicios de matrices en c# y Python

---
+++ 03_Arreglos/Matrices/C#/Ejemplo01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Tablero de juego (ej. Tres en Raya): Un programa que simula el 
+            tablero de un juego simple como el Tres en Raya, permitiendo mostrar el estado 
+            actual del tablero.
+             */
+            char[,] tablero = new char[3, 3]
+                {
+                { 'X', 'O', 'X' },
+                { ' ', 'X', ' ' },
+                { 'O', ' ', 'O' }
+            };
+            Console.WriteLine("Estado actual del tablero de Tres en Raya:");
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    Console.Write(tablero[i, j]);
+                    if (j < tablero.GetLength(1) - 1)
+                        Console.Write(" | ");
+                }
+                Console.WriteLine();
+                if (i < tablero.GetLength(0) - 1)
+                    Console.WriteLine("---------");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 03_Arreglos/Matrices/C#/Ejemplo02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Inventario de almacén por pasillo y estante: 
+            Un programa que usa una matriz para representar la
+            cantidad de productos en un almacén, organizados por pasillo (filas) y
+            estante (columnas).
+             */
+            int[,] inventario = new int[3, 4]
+                {
+                { 10, 5, 8, 12 },
+                { 7, 14, 6, 9 },
+                { 20, 15, 10, 5 }
+                    };
+            Console.WriteLine("Inventario del almacén (Pasillos x Estantes):");
+            for (int i = 0; i < inventario.GetLength(0); i++)
+            {
+                for (int j = 0; j < inventario.GetLength(1); j++)
+                {
+                    Console.Write(inventario[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
